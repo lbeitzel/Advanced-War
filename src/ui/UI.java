6d52fc90ec177bab47b040a9fb4b46b2a33990eb
Lucas Beitzel <lucasbei@buffalo.edu>
2017-07-25 23:24:31 -0400

    Basic Deck and UI Classes

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -2,10 +2,15 @@ package ui;
 
 import javax.swing.*;
 
+import model.Deck;
+
 public class UI extends JFrame {
 	public UI() {
+		Deck d = new Deck();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		JLabel label = new JLabel();
+		System.out.println("\u2666");
+		d.shuffle();
+		JLabel label = new JLabel(d.toString());
 		add(label);
 		pack();
 		setVisible(true);
